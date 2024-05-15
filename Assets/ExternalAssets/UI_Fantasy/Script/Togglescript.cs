f25681b906c26697c2575388287083b33120b158
orlo <oguerrerofarias@gmail.com>
2024-05-15 17:02:32 -0400

    First commit. 1. ArtBaseAssets 2. NavPackage. 3.Simple Scene First Iteration

---
+++ Assets/ExternalAssets/UI_Fantasy/Script/Togglescript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ExternalAssets.UI_Fantasy.Script
+{
+    public class Togglescript : MonoBehaviour {
+
+        Toggle toggle;
+
+        private void Start()
+        {
+            toggle = GetComponent<Toggle>();
+        }
+
+        public GameObject Slider;
+
+
+        private void Update()
+        {
+            if (toggle.isOn)
+            {
+                Slider.SetActive(false);
+            }
+            else
+            {
+                Slider.SetActive(true);
+            }
+        }
+    }
+}
